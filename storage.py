c6b4b726b4c20c70e318f8012f7b4b1bb69c1c15
Veronika Kashlej <veronika.kashlei@mail.ru>
2025-05-09 12:25:51 +0300

    feat: add project

---
+++ storage.py
@@ -0,0 +1,82 @@
+from typing import Optional
+from fastapi import FastAPI, UploadFile, File, HTTPException
+from fastapi.responses import FileResponse, JSONResponse, Response
+from pathlib import Path
+from datetime import datetime
+import shutil
+
+app = FastAPI()
+storage_dir = Path("storage")
+
+storage_dir.mkdir(exist_ok=True)
+
+def get_full_path(path: str) -> Path:
+    safe_path = (storage_dir / path).resolve()
+    if not safe_path.is_relative_to(storage_dir.resolve()):
+        raise HTTPException(status_code=400, detail="Invalid path")
+    return safe_path
+
+@app.put("/files/{path:path}")
+async def upload_file(
+    path: str,
+    file: UploadFile = File(...) 
+):
+    full_path = get_full_path(path)
+    full_path.parent.mkdir(parents=True, exist_ok=True)
+    
+    with open(full_path, "wb") as f:
+        f.write(await file.read())
+    return Response(status_code=201, content="File was uploaded")
+
+@app.get("/files/{path:path}")
+def get_file_or_dir(path: str):
+    full_path = get_full_path(path)
+    if not full_path.exists():
+        raise HTTPException(status_code=404, detail="Not found")
+    
+    if full_path.is_file():
+        return FileResponse(full_path, media_type="application/octet-stream", filename=full_path.name)
+
+    if full_path.is_dir():
+        files = []
+        directories = []
+        for item in full_path.iterdir():
+            if item.is_file():
+                files.append(item.name)
+            elif item.is_dir():
+                directories.append(item.name)
+        return JSONResponse(
+            status_code=200,
+            content={
+                "files": files,
+                "directories": directories
+            }
+        )
+    
+@app.head("/files/{path:path}")
+def get_file_info(path: str):
+    full_path = get_full_path(path)
+    if not full_path.is_file():
+        raise HTTPException(status_code=404, detail="File not found")
+    
+    stat = full_path.stat()
+    headers = {
+        "Content-Length": str(stat.st_size),
+        "Last-Modified": str(datetime.utcfromtimestamp(stat.st_mtime)),
+    }
+    return Response(headers=headers)
+
+@app.delete("/files/{path:path}")
+def delete_path(path: str):
+    full_path = get_full_path(path)
+    if not full_path.exists():
+        raise HTTPException(status_code=404, detail="Not found")
+    
+    try: 
+        if full_path.is_file():
+            full_path.unlink()  
+        else:
+            shutil.rmtree(full_path)
+        return Response(status_code=204)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error: {e}")
